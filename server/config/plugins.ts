35109399f3772352c551c8dfb5aca686b6b9fd98
tyrontyc <tyron.c0305@gmail.com>
2025-03-07 16:34:35 -0500

    user permissions JWT for strapi

--- server/config/plugins.ts
+++ server/config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+export default ({ env }) => ({
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET", "eed1cf06a0cdbc12f28c486c73700f80"),
+    },
+  },
+});
